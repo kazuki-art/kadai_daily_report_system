708b20781d8ebca713072d38c8329b833c606e26
kazuki.sato <toaru1018@yahoo.co.jp>
2020-06-04 12:42:37 +0900

    Add Employee index

--- src/models/validators/EmployeeValidator.java
+++ src/models/validators/EmployeeValidator.java
@@ -28,48 +28,46 @@ public class EmployeeValidator {
         }
 
         return errors;
-
     }
 
 
 
-    private static String _validatePassword(String password, Boolean password_check_flag) {
-        // TODO 自動生成されたメソッド・スタブ
-        if(password == null || password.equals("")) {
-            return "パスワードを入力してください。";
-        }
-        return "";
-    }
-
- // 社員名の必須入力チェック
-
-    private static String _validateName(String name) {
-        // TODO 自動生成されたメソッド・スタブ
-        if(name == null || name.equals("")) {
-            return "氏名を入力してください。";
-        }
-        return "";
-    }
-
-  //社員番号
-
+ // 社員番号
     private static String _validateCode(String code, Boolean code_duplicate_check_flag) {
-        // TODO 自動生成されたメソッド・スタブ
+        // 必須入力チェック
         if(code == null || code.equals("")) {
             return "社員番号を入力してください。";
         }
 
-        //すでに登録されている社員番号との重複チェック
+        // すでに登録されている社員番号との重複チェック
         if(code_duplicate_check_flag) {
             EntityManager em = DBUtil.createEntityManager();
             long employees_count = (long)em.createNamedQuery("checkRegisteredCode", Long.class)
-                    .setParameter("code", code)
-                    .getSingleResult();
+                                           .setParameter("code", code)
+                                             .getSingleResult();
             em.close();
             if(employees_count > 0) {
                 return "入力された社員番号の情報はすでに存在しています。";
             }
+        }
 
+        return "";
+    }
+
+    // 社員名の必須入力チェック
+    private static String _validateName(String name) {
+        if(name == null || name.equals("")) {
+            return "氏名を入力してください。";
+        }
+
+        return "";
+    }
+
+    // パスワードの必須入力チェック
+    private static String _validatePassword(String password, Boolean password_check_flag) {
+        // パスワードを変更する場合のみ実行
+        if(password_check_flag && (password == null || password.equals(""))) {
+            return "パスワードを入力してください。";
         }
         return "";
     }
